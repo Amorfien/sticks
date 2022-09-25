ab807094f5d39828a6f3d7ecff3cf2b05a0a27bb
Pavel Grigorev <amorfien@yandex.ru>
2022-09-25 17:36:16 +0300

    Initial Commit

---
+++ sticks/main.swift
@@ -0,0 +1,66 @@
+//
+//  main.swift
+//  sticks
+//
+//  Created by Pavel Grigorev on 20.09.2022.
+//
+
+import Foundation
+
+let cat = """
+
+　　　　　／＞ 　  フ
+　　　　　| 　_　 _|
+　 　　　／`ミ _x 彡
+　　 　 /　　　 　 |
+　　　 /　 ヽ　　 ﾉ
+　／￣|　　 |　|　|
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+"""
+
+var sticks: [Character] = []
+var count: UInt8 = 0
+var player = true
+var win = false
+
+print("Введите количество палочек от 5 до \(UInt8.max)")
+count = UInt8(readLine()!) ?? 0
+
+guard count>4 && count<=255 else {
+    print("Вы ввели неверное число")
+    exit(1)
+}
+sticks = Array(repeating: "|", count: Int(count))
+print("\n\(sticks)\n")
+
+func removing() {
+    switch player {
+    case true: print("\nИГРОК 1")
+    case false: print("\nИГРОК 2")
+    }
+    if sticks.count == 1 { loosing() }
+    print("Вытащите от 1 до 3 палочек")
+    let remove = Int(readLine()!) ?? 0
+    guard remove>0 && remove<=3 else {
+        print("\nВы ввели неверное число")
+        exit(2)
+    }
+    if remove < sticks.count {
+        sticks.removeLast(remove)
+        print("\n\(sticks)")
+        player.toggle()
+    } else { loosing() }
+
+}
+
+while sticks.count > 0 {
+    removing()
+}
+
+func loosing() {
+    print("   YOU LOOSE!\n")
+    print(cat)
+    exit(4)
+}
